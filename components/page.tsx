7234de5295f7ea677aa617e86320ecab4860e8e2
Hasu <hshue83@gmail.com>
2022-02-24 12:27:43 +0900

    feat: update base layout

--- components/page.tsx
+++ components/page.tsx
@@ -1,11 +1,35 @@
 import type { NextPage } from "next";
+import { ReactNode } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
-const Main: NextPage = ({ children }) => {
+type Props = {
+  pageTitle?: string;
+  children: ReactNode;
+};
+const Main = ({ pageTitle, children }: Props) => {
   return (
     <div style={{ height: "100vh" }}>
       <Header />
-      <div style={{ backgroundColor: "#3AC9D2", height: "60vh" }}></div>
+      <div
+        style={{
+          backgroundColor: "#3AC9D2",
+          height: "60vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h3
+          style={{
+            fontSize: "4rem",
+            textAlign: "center",
+            fontWeight: "bold",
+            letterSpacing: "0.88rem",
+          }}
+        >
+          {pageTitle}
+        </h3>
+      </div>
       {children}
       <Footer />
     </div>

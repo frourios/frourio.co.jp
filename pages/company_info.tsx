29ee1d9f8b26380416bc6fe1fac0f34e8e717972
Hasu <hshue83@gmail.com>
2022-02-24 11:21:20 +0900

    feat: update company info

--- pages/company_info.tsx
+++ pages/company_info.tsx
@@ -16,9 +16,10 @@ const Main = () => {
       <Table maxW="900px" m="15rem auto" w="80%" fontSize="1.8rem">
         <Tbody>
           <Item sub="会社名" val="フルーリオ株式会社" />
-          <Item sub="所在地" val="東京都北区赤羽2-4-14 4A" />
           <Item sub="代表取締役" val="松田 光秀" />
+          <Item sub="所在地" val="東京都北区赤羽2-4-14 4A" />
           <Item sub="資本金" val="10万円" />
+          <Item sub="事業内容" val="OSSの開発運用保守" />
         </Tbody>
       </Table>
     </Layout>

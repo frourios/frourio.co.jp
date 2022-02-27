3a547e382186736fa0a27331d44fc6b6ce7fd7d3
Hasu <hshue83@gmail.com>
2022-02-27 20:10:58 +0900

    style: add line-height to wrap in table

--- pages/company_info.tsx
+++ pages/company_info.tsx
@@ -4,8 +4,12 @@ import Layout from "../components/page";
 const Item = (props: { sub: string; val: string }) => {
   return (
     <Tr>
-      <Td padding="4rem 0">{props.sub}</Td>
-      <Td>{props.val}</Td>
+      <Td borderColor="gray.300" padding="4rem 0" lineHeight="1.5em">
+        {props.sub}
+      </Td>
+      <Td borderColor="gray.300" lineHeight="1.5em">
+        {props.val}
+      </Td>
     </Tr>
   );
 };
@@ -13,13 +17,22 @@ const Item = (props: { sub: string; val: string }) => {
 const Main = () => {
   return (
     <Layout pageTitle="会社情報">
-      <Table maxW="900px" m="15rem auto" w="80%" fontSize="1.8rem">
+      <Table
+        maxW="900px"
+        m="15rem auto"
+        w="80%"
+        fontSize={{ base: "1.6rem", md: "1.8rem" }}
+        lineHeight="1.5em"
+      >
         <Tbody>
           <Item sub="会社名" val="フルーリオ株式会社" />
           <Item sub="代表取締役" val="松田 光秀" />
           <Item sub="所在地" val="東京都北区赤羽2-4-14 4A" />
           <Item sub="資本金" val="10万円" />
-          <Item sub="事業内容" val="OSSの開発運用保守" />
+          <Item
+            sub="事業内容"
+            val="ソフトウェアの研究開発、提供、技術指導及びコンサルタント業"
+          />
         </Tbody>
       </Table>
     </Layout>

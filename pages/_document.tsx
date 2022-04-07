2d068924a6f7ddcf6faba74a4c9bfd91f34189de
Hasu <hshue83@gmail.com>
2022-04-08 08:35:30 +0900

    feat: add OGP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 function MyLayout() {
+  const baseUrl = "https://frourio.co.jp";
   return (
     <Html>
       <Head>
         <link rel="icon" href="favicon.svg" sizes="64x64" />
-        <meta property="og:image" content="ogp.png" />
+        <meta property="og:image" content={`${baseUrl}/ogp.png`} />
       </Head>
       <body>
         <Main />

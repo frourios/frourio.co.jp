35c84a3e3c4a7444acf36cf3c4b3f854738b5ba4
Hasu <hshue83@gmail.com>
2022-04-08 08:15:32 +0900

    feat: add OGP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ function MyLayout() {
     <Html>
       <Head>
         <link rel="icon" href="favicon.svg" sizes="64x64" />
+        <meta property="og:image" content="ogp.png" />
       </Head>
       <body>
         <Main />
